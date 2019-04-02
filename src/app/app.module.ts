18f5d759934ca184c800f56e1b3e819470cd9855
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-04-01 20:51:41 -0400

    lay navbar base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SplashComponent } from './components/splash/splash.component';
+import { ServicesComponentComponent } from './components/services-component/services-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SplashComponent
+    SplashComponent,
+    ServicesComponentComponent
   ],
   imports: [
     BrowserModule,
